48e8acf36b3dd85b843c8b282a8e47599e586a44
AlexAviles345 <20233tn198@utez.edu.mx>
2025-05-06 14:30:21 -0500

    [ADD] Entidad cliente

---
+++ GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteEntity.java
@@ -0,0 +1,83 @@
+package utez.edu.mx.GestionClientes.clientes;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "")
+public class ClienteEntity {
+
+    // ATRIBUTOS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(name = "apellido_paterno", nullable = false)
+    private String apellidoPaterno;
+
+    @Column(name = "apellido_materno")
+    private String apellidoMaterno;
+
+    @Column(nullable = false, unique = true)
+    private String curp;
+
+    @Column(name = "fecha_nacimiento")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaNacimiento;
+
+
+
+    // GETTERS Y SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
---
+++ GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteRepository.java
@@ -0,0 +1,2 @@
+package utez.edu.mx.GestionClientes.clientes;public class ClienteRepository {
+}
