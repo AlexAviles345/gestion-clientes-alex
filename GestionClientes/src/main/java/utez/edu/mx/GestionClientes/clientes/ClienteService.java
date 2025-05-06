12be773b13ea1427cd2a94a8ab18162737e90747
AlexAviles345 <20233tn198@utez.edu.mx>
2025-05-06 14:42:58 -0500

    [ADD] Service de cliente con metodo getAll

--- GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteService.java
+++ GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteService.java
@@ -1,2 +1,21 @@
-package utez.edu.mx.GestionClientes.clientes;public class ClienteService {
+package utez.edu.mx.GestionClientes.clientes;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Transactional(rollbackFor = {SQLException.class, Exception.java})
+    public List<ClienteEntity> getAllClientes(){
+        return clienteRepository.findAll();
+    }
 }
