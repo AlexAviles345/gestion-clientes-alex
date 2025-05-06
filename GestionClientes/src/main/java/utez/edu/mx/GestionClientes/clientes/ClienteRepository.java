d7f126f388d89a186fc4cee4cdacb22ad47d2634
AlexAviles345 <20233tn198@utez.edu.mx>
2025-05-06 14:37:26 -0500

    [ADD] Repository de cliente

--- GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteRepository.java
+++ GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteRepository.java
@@ -1,2 +1,8 @@
-package utez.edu.mx.GestionClientes.clientes;public class ClienteRepository {
+package utez.edu.mx.GestionClientes.clientes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
 }
