949b8ee231a3a5cf599adb1a746e79d5bcfe1da5
AlexAviles345 <20233tn198@utez.edu.mx>
2025-05-06 15:39:27 -0500

    [ADD] Endpoints de actualizar y eliminar por id cliente

--- GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteController.java
+++ GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteController.java
@@ -1,2 +1,42 @@
-package utez.edu.mx.GestionClientes.clientes;public class ClienteController {
+package utez.edu.mx.GestionClientes.clientes;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/get-all")
+    public List<ClienteEntity> getAllClientes(){
+        return clienteService.getAllClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getByIdCliente(@PathVariable Long id){
+        return clienteService.getClienteById(id);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> getByIdCliente(@RequestBody ClienteEntity cliente){
+        return clienteService.addCliente(cliente);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateByIdCliente(@RequestBody ClienteEntity cliente){
+        return clienteService.actualizarCliente(cliente);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteByIdCliente(@PathVariable Long id){
+        return clienteService.eliminarClienteById(id);
+    }
+
 }
--- GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteService.java
+++ GestionClientes/src/main/java/utez/edu/mx/GestionClientes/clientes/ClienteService.java
@@ -2,11 +2,18 @@ package utez.edu.mx.GestionClientes.clientes;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -14,8 +21,87 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @Transactional(rollbackFor = {SQLException.class, Exception.java})
+    private Map<String, Object> body;
+    private HttpStatus status;
+
+    @Transactional( readOnly = true )
     public List<ClienteEntity> getAllClientes(){
         return clienteRepository.findAll();
     }
+
+    @Transactional( readOnly = true )
+    public ResponseEntity<?> getClienteById(Long id){
+        Optional<ClienteEntity> cliente = clienteRepository.findById(id);
+        body = new HashMap<>();
+
+        if(cliente.isPresent()){
+            body.put("datos", cliente.get());
+            status = HttpStatus.OK;
+        }
+        else {
+            body.put("mensaje", "Cliente no encontrado");
+            status = HttpStatus.NOT_FOUND;
+        }
+
+        return new ResponseEntity<>(body, status);
+    }
+
+    @Transactional(rollbackFor = {SQLException.class, Exception.class})
+    public ResponseEntity<?> addCliente(ClienteEntity cliente){
+
+        try {
+
+            clienteRepository.save(cliente);
+
+            body.put("datos", cliente);
+            body.put("mensaje", "El cliente se ha guardado correctamente");
+            status = HttpStatus.OK;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(body, status);
+    }
+
+    @Transactional(rollbackFor = {SQLException.class, Exception.class})
+    public ResponseEntity<?> actualizarCliente(ClienteEntity clienteUpdate){
+        Optional<ClienteEntity> cliente = clienteRepository.findById(clienteUpdate.getId());
+
+        if(cliente.isPresent()){
+            try {
+
+                clienteRepository.save(clienteUpdate);
+
+                body.put("datos", clienteUpdate);
+                body.put("mensaje", "El cliente se ha actualizado correctamente");
+                status = HttpStatus.OK;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        return new ResponseEntity<>(body, status);
+    }
+
+    @Transactional( readOnly = true )
+    public ResponseEntity<?> eliminarClienteById(Long id){
+        Optional<ClienteEntity> cliente = clienteRepository.findById(id);
+        body = new HashMap<>();
+
+        if(cliente.isPresent()){
+            body.put("mensaje", "El cliente se ha eliminado correctamente");
+            status = HttpStatus.OK;
+        }
+        else {
+            body.put("mensaje", "Cliente no encontrado");
+            status = HttpStatus.NOT_FOUND;
+        }
+
+        return new ResponseEntity<>(body, status);
+    }
+
 }
